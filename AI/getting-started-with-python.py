bf14f8f4c4744618b98b1d5b2997503a3e92281f
KSingh <ksinghofficial@skiff.com>
2023-08-31 00:41:51 +0530

    Update and rename main.py to getting-started-with-python.py

---
+++ AI/getting-started-with-python.py
@@ -0,0 +1,86 @@
+ print("hello world")
+ print("your_name")
+# string
+ name = "your_name"
+ age = "your_age"
+# float
+ name = "your_name"
+ age = "your_age"
+ print(name)
+ print(age)
+
+ name = "your_name"
+ age = your_age
+ line = "ur genius"
+ print(name)
+ print(age)
+ print(line)
+ print("your_name")
+ name = "your_name"
+ print(name)
+ age = your_age
+ print(age)
+ line = "ur sexy"
+ print(line)
+
+
+ input("what is ur name")
+ name = input("what is ur name")
+ print("Hello"  +   name)
+ nickname = input("what is ur nick name")
+ print("Ur nickname is" + nickname)
+ old_age = input("enter ur old age : ")
+ new_age = int(old_age) + 10
+ print(new_age)
+
+# float() decimal
+# str()
+# bool()
+
+ no = integer
+ print(float(integer))
+
+ first = input("enter first no :")
+ second = input("enter second no :")
+ sum = int(first) + int(second)
+ print("the sum is : " + str(sum))
+
+ name = "your_name"
+ print(name.upper())
+ name2 = "your_name"
+ print(name2.lower())
+
+# -1 means does not exist
+# numbering in python starts from 0
+
+ name= "your_name"
+ print(name.find("letteroflastnameincapital"))
+ print(name.find("last_name_startswithcapital"))
+ print(name.find("last_name_notstartswithcapital"))
+ print(name.find("letteroffirstnameinnoncapital"))
+
+ name = "your_name"
+ print(name.replace("your_nickname","your_fullname"))
+ print(name.replace("your_fullname", "your_nickname"))
+ print(name)
+
+# to check existance OUTPUT IN TRUE OR FALSE
+ name = "your_name"
+ print("letterthatexistsinurname" in name)
+ print("letterthatdonotexistsinurname" in name)
+
+# arethamatic operession
+print(any_number+any_number)
+print(any_number-any_number)
+print(any_number*any_number)
+print(any_number/any_number)
+# to get no decimal after
+print(any_number//any_number)
+# to get reminder
+print(any_number % any_number)
+# power
+print(any_number**any_number)
+i = number_x
+i = i + number_y
+i += number_y
+
