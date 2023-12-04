03f6a47afbaf0183b35c05598934f61e19e3926c
Kamlendra Singh <ksinghofficial@proton.me>
2023-12-04 16:54:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,20 @@ else :
      else:
         max=c
         print("largest number is", max)
+#Program to display the Fibonacci sequence up to n-th term.
+ntearms = int(input("how many tearms? "))
+n1, n2 = 0,1
+count = 0
+if ntearms<= 0:
+    print("please enter a positive integer")
+elif ntearms == 1:
+        print("Fibonacci sequence upto", ntearms,":")
+        print(n1)
+else:
+            print("Fibonacci sequence:")
+while count < ntearms:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2= nth
+        count += 1         
